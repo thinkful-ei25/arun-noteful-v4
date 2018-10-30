bf1243d166d79ba292204b4effcd1d12e298542c
Arun Seehra <seehra.a@gmail.com>
2018-10-30 14:20:24 -0500

    Add ability to refresh auth tokens

--- routes/auth.js
+++ routes/auth.js
@@ -4,12 +4,18 @@ const express = require('express');
 const passport = require('passport');
 
 const { createAuthToken } = require('../auth/tokens');
+const tokenAuth = require('../auth/tokenAuth');
 
 const router = new express.Router();
 const authOptions = { session: false, failWithError: true };
 
 router.post('/login', passport.authenticate('local', authOptions), (req, res) => {
   const authToken = createAuthToken(req.user.toObject());
+  res.json({ authToken });
+});
+
+router.post('/refresh', tokenAuth, (req, res) => {
+  const authToken = createAuthToken(req.user);
   res.json({ authToken });
 });
 
