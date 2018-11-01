9f11e6e80831a0485cdc977468d063083ba206a1
Arun Seehra <seehra.a@gmail.com>
2018-11-01 10:26:41 -0500

    Prettify test/tags.test.js and use helper utils

--- test/tags.test.js
+++ test/tags.test.js
@@ -1,110 +1,84 @@
-/* eslint-disable */
 'use strict';
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const mongoose = require('mongoose');
 const express = require('express');
 const sinon = require('sinon');
 
 const app = require('../server');
 const Tag = require('../models/tag');
 const Note = require('../models/note');
-const { notes, tags } = require('../db/data');
-const { TEST_MONGODB_URI } = require('../config');
+const utils = require('./utils');
 
 chai.use(chaiHttp);
-const expect = chai.expect;
+const { expect } = chai;
 const sandbox = sinon.createSandbox();
 
 describe('Noteful API - Tags', function () {
+  before(() => utils.connectToDatabase());
+  after(() => utils.disconnectFromDatabase());
 
-  before(function () {
-    return mongoose.connect(TEST_MONGODB_URI, { useNewUrlParser: true, useCreateIndex : true })
-      .then(() => Promise.all([
-        Note.deleteMany(),
-        Tag.deleteMany()
-      ]));
-  });
-
-  beforeEach(function () {
-    return Promise.all([
-      Tag.insertMany(tags),
-      Note.insertMany(notes)
-    ]);
-  });
-
-  afterEach(function () {
+  beforeEach(() => utils.seedDatabase());
+  afterEach(() => {
     sandbox.restore();
-    return Promise.all([
-      Note.deleteMany(),
-      Tag.deleteMany()
-    ]);
-  });
-
-  after(function () {
-    return mongoose.disconnect();
+    return utils.cleanDatabase();
   });
 
   describe('GET /api/tags', function () {
-
     it('should return the correct number of tags', function () {
-      return Promise.all([
-        Tag.find(),
-        chai.request(app).get('/api/tags')
-      ])
-        .then(([data, res]) => {
+      return Promise.all([Tag.find(), chai.request(app).get('/api/tags')]).then(
+        ([data, res]) => {
           expect(res).to.have.status(200);
           expect(res).to.be.json;
           expect(res.body).to.be.a('array');
           expect(res.body).to.have.length(data.length);
-        });
+        },
+      );
     });
 
+    // eslint-disable-next-line max-len
     it('should return a list sorted by name with the correct fields and values', function () {
       return Promise.all([
         Tag.find().sort('name'),
-        chai.request(app).get('/api/tags')
-      ])
-        .then(([data, res]) => {
-          expect(res).to.have.status(200);
-          expect(res).to.be.json;
-          expect(res.body).to.be.a('array');
-          expect(res.body).to.have.length(data.length);
-          res.body.forEach(function (item, i) {
-            expect(item).to.be.a('object');
-            expect(item).to.have.all.keys('id', 'name', 'createdAt', 'updatedAt');
-            expect(item.id).to.equal(data[i].id);
-            expect(item.name).to.equal(data[i].name);
-            expect(new Date(item.createdAt)).to.eql(data[i].createdAt);
-            expect(new Date(item.updatedAt)).to.eql(data[i].updatedAt);
-          });
+        chai.request(app).get('/api/tags'),
+      ]).then(([data, res]) => {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('array');
+        expect(res.body).to.have.length(data.length);
+        res.body.forEach(function (item, i) {
+          expect(item).to.be.a('object');
+          expect(item).to.have.all.keys('id', 'name', 'createdAt', 'updatedAt');
+          expect(item.id).to.equal(data[i].id);
+          expect(item.name).to.equal(data[i].name);
+          expect(new Date(item.createdAt)).to.eql(data[i].createdAt);
+          expect(new Date(item.updatedAt)).to.eql(data[i].updatedAt);
         });
+      });
     });
 
     it('should catch errors and respond properly', function () {
       sandbox.stub(Tag.schema.options.toJSON, 'transform').throws('FakeError');
 
-      return chai.request(app).get('/api/tags')
-        .then(res => {
+      return chai
+        .request(app)
+        .get('/api/tags')
+        .then((res) => {
           expect(res).to.have.status(500);
           expect(res).to.be.json;
           expect(res.body).to.be.a('object');
           expect(res.body.message).to.equal('Internal Server Error');
         });
     });
-
   });
 
   describe('GET /api/tags/:id', function () {
-
     it('should return correct tags', function () {
       let data;
       return Tag.findOne()
-        .then(_data => {
+        .then((_data) => {
           data = _data;
-          return chai.request(app)
-            .get(`/api/tags/${data.id}`);
+          return chai.request(app).get(`/api/tags/${data.id}`);
         })
         .then((res) => {
           expect(res).to.have.status(200);
@@ -119,9 +93,10 @@ describe('Noteful API - Tags', function () {
     });
 
     it('should respond with a 400 for an invalid id', function () {
-      return chai.request(app)
+      return chai
+        .request(app)
         .get('/api/tags/NOT-A-VALID-ID')
-        .then(res => {
+        .then((res) => {
           expect(res).to.have.status(400);
           expect(res.body.message).to.equal('The `id` is not valid');
         });
@@ -129,9 +104,10 @@ describe('Noteful API - Tags', function () {
 
     it('should respond with a 404 for an id that does not exist', function () {
       // The string "DOESNOTEXIST" is 12 bytes which is a valid Mongo ObjectId
-      return chai.request(app)
+      return chai
+        .request(app)
         .get('/api/tags/DOESNOTEXIST')
-        .then(res => {
+        .then((res) => {
           expect(res).to.have.status(404);
         });
     });
@@ -140,29 +116,26 @@ describe('Noteful API - Tags', function () {
       sandbox.stub(Tag.schema.options.toJSON, 'transform').throws('FakeError');
 
       return Tag.findOne()
-        .then(data => {
-          return chai.request(app).get(`/api/tags/${data.id}`);
-        })
-        .then(res => {
+        .then(data => chai.request(app).get(`/api/tags/${data.id}`))
+        .then((res) => {
           expect(res).to.have.status(500);
           expect(res).to.be.json;
           expect(res.body).to.be.a('object');
           expect(res.body.message).to.equal('Internal Server Error');
         });
     });
-
   });
 
   describe('POST /api/tags', function () {
-
     it('should create and return a new item when provided valid data', function () {
       const newItem = { name: 'newTag' };
       let body;
-      return chai.request(app)
+      return chai
+        .request(app)
         .post('/api/tags')
         .send(newItem)
         .then(function (res) {
-          body = res.body;
+          ({ body } = res);
           expect(res).to.have.status(201);
           expect(res).to.have.header('location');
           expect(res).to.be.json;
@@ -170,7 +143,7 @@ describe('Noteful API - Tags', function () {
           expect(body).to.have.all.keys('id', 'name', 'createdAt', 'updatedAt');
           return Tag.findOne({ _id: body.id });
         })
-        .then(data => {
+        .then((data) => {
           expect(body.id).to.equal(data.id);
           expect(body.name).to.equal(data.name);
           expect(new Date(body.createdAt)).to.eql(data.createdAt);
@@ -180,10 +153,11 @@ describe('Noteful API - Tags', function () {
 
     it('should return an error when missing "name" field', function () {
       const newItem = {};
-      return chai.request(app)
+      return chai
+        .request(app)
         .post('/api/tags')
         .send(newItem)
-        .then(res => {
+        .then((res) => {
           expect(res).to.have.status(400);
           expect(res).to.be.json;
           expect(res.body).to.be.a('object');
@@ -193,10 +167,11 @@ describe('Noteful API - Tags', function () {
 
     it('should return an error when "name" field is empty string', function () {
       const newItem = { name: '' };
-      return chai.request(app)
+      return chai
+        .request(app)
         .post('/api/tags')
         .send(newItem)
-        .then(res => {
+        .then((res) => {
           expect(res).to.have.status(400);
           expect(res).to.be.json;
           expect(res.body).to.be.a('object');
@@ -206,13 +181,14 @@ describe('Noteful API - Tags', function () {
 
     it('should return an error when given a duplicate name', function () {
       return Tag.findOne()
-        .then(data => {
+        .then((data) => {
           const newItem = { name: data.name };
-          return chai.request(app)
+          return chai
+            .request(app)
             .post('/api/tags')
             .send(newItem);
         })
-        .then(res => {
+        .then((res) => {
           expect(res).to.have.status(400);
           expect(res).to.be.json;
           expect(res.body).to.be.a('object');
@@ -224,28 +200,28 @@ describe('Noteful API - Tags', function () {
       sandbox.stub(Tag.schema.options.toJSON, 'transform').throws('FakeError');
 
       const newItem = { name: 'newTag' };
-      return chai.request(app)
+      return chai
+        .request(app)
         .post('/api/tags')
         .send(newItem)
-        .then(res => {
+        .then((res) => {
           expect(res).to.have.status(500);
           expect(res).to.be.json;
           expect(res.body).to.be.a('object');
           expect(res.body.message).to.equal('Internal Server Error');
         });
     });
-
   });
 
   describe('PUT /api/tags/:id', function () {
-
     it('should update the tag', function () {
       const updateItem = { name: 'Updated Name' };
       let data;
       return Tag.findOne()
-        .then(_data => {
+        .then((_data) => {
           data = _data;
-          return chai.request(app)
+          return chai
+            .request(app)
             .put(`/api/tags/${data.id}`)
             .send(updateItem);
         })
@@ -264,10 +240,11 @@ describe('Noteful API - Tags', function () {
 
     it('should respond with a 400 for an invalid id', function () {
       const updateItem = { name: 'Blah' };
-      return chai.request(app)
+      return chai
+        .request(app)
         .put('/api/tags/NOT-A-VALID-ID')
         .send(updateItem)
-        .then(res => {
+        .then((res) => {
           expect(res).to.have.status(400);
           expect(res.body.message).to.equal('The `id` is not valid');
         });
@@ -276,10 +253,11 @@ describe('Noteful API - Tags', function () {
     it('should respond with a 404 for an id that does not exist', function () {
       const updateItem = { name: 'Blah' };
       // The string "DOESNOTEXIST" is 12 bytes which is a valid Mongo ObjectId
-      return chai.request(app)
+      return chai
+        .request(app)
         .put('/api/tags/DOESNOTEXIST')
         .send(updateItem)
-        .then(res => {
+        .then((res) => {
           expect(res).to.have.status(404);
         });
     });
@@ -288,13 +266,14 @@ describe('Noteful API - Tags', function () {
       const updateItem = {};
       let data;
       return Tag.findOne()
-        .then(_data => {
+        .then((_data) => {
           data = _data;
-          return chai.request(app)
+          return chai
+            .request(app)
             .put(`/api/tags/${data.id}`)
             .send(updateItem);
         })
-        .then(res => {
+        .then((res) => {
           expect(res).to.have.status(400);
           expect(res).to.be.json;
           expect(res.body).to.be.a('object');
@@ -306,13 +285,14 @@ describe('Noteful API - Tags', function () {
       const updateItem = { name: '' };
       let data;
       return Tag.findOne()
-        .then(_data => {
+        .then((_data) => {
           data = _data;
-          return chai.request(app)
+          return chai
+            .request(app)
             .put(`/api/tags/${data.id}`)
             .send(updateItem);
         })
-        .then(res => {
+        .then((res) => {
           expect(res).to.have.status(400);
           expect(res).to.be.json;
           expect(res.body).to.be.a('object');
@@ -321,15 +301,17 @@ describe('Noteful API - Tags', function () {
     });
 
     it('should return an error when given a duplicate name', function () {
-      return Tag.find().limit(2)
-        .then(results => {
+      return Tag.find()
+        .limit(2)
+        .then((results) => {
           const [item1, item2] = results;
           item1.name = item2.name;
-          return chai.request(app)
+          return chai
+            .request(app)
             .put(`/api/tags/${item1.id}`)
             .send(item1);
         })
-        .then(res => {
+        .then((res) => {
           expect(res).to.have.status(400);
           expect(res).to.be.json;
           expect(res.body).to.be.a('object');
@@ -342,37 +324,33 @@ describe('Noteful API - Tags', function () {
 
       const updateItem = { name: 'Updated Name' };
       return Tag.findOne()
-        .then(data => {
-          return chai.request(app)
-            .put(`/api/tags/${data.id}`)
-            .send(updateItem);
-        })
-        .then(res => {
+        .then(data => chai
+          .request(app)
+          .put(`/api/tags/${data.id}`)
+          .send(updateItem))
+        .then((res) => {
           expect(res).to.have.status(500);
           expect(res).to.be.json;
           expect(res.body).to.be.a('object');
           expect(res.body.message).to.equal('Internal Server Error');
         });
     });
-
   });
 
   describe('DELETE /api/tags/:id', function () {
-
     it('should delete an existing tag and respond with 204', function () {
       let data;
       return Tag.findOne()
-        .then(_data => {
+        .then((_data) => {
           data = _data;
-          return chai.request(app)
-            .delete(`/api/tags/${data.id}`);
+          return chai.request(app).delete(`/api/tags/${data.id}`);
         })
         .then(function (res) {
           expect(res).to.have.status(204);
           expect(res.body).to.be.empty;
           return Tag.count({ _id: data.id });
         })
-        .then(count => {
+        .then((count) => {
           expect(count).to.equal(0);
         });
     });
@@ -380,26 +358,26 @@ describe('Noteful API - Tags', function () {
     it('should delete an existing tag and remove tag reference from note', function () {
       let tagId;
       return Note.findOne({ tags: { $exists: true, $ne: [] } })
-        .then(data => {
-          tagId = data.tags[0];
+        .then((data) => {
+          [tagId] = data;
 
-          return chai.request(app)
-            .delete(`/api/tags/${tagId}`);
+          return chai.request(app).delete(`/api/tags/${tagId}`);
         })
         .then(function (res) {
           expect(res).to.have.status(204);
           expect(res.body).to.be.empty;
           return Note.count({ tags: tagId });
         })
-        .then(count => {
+        .then((count) => {
           expect(count).to.equal(0);
         });
     });
 
     it('should respond with a 400 for an invalid id', function () {
-      return chai.request(app)
+      return chai
+        .request(app)
         .delete('/api/tags/NOT-A-VALID-ID')
-        .then(res => {
+        .then((res) => {
           expect(res).to.have.status(400);
           expect(res.body.message).to.equal('The `id` is not valid');
         });
@@ -408,17 +386,13 @@ describe('Noteful API - Tags', function () {
     it('should catch errors and respond properly', function () {
       sandbox.stub(express.response, 'sendStatus').throws('FakeError');
       return Tag.findOne()
-        .then(data => {
-          return chai.request(app).delete(`/api/tags/${data.id}`);
-        })
-        .then(res => {
+        .then(data => chai.request(app).delete(`/api/tags/${data.id}`))
+        .then((res) => {
           expect(res).to.have.status(500);
           expect(res).to.be.json;
           expect(res.body).to.be.a('object');
           expect(res.body.message).to.equal('Internal Server Error');
         });
     });
-
   });
-
 });
